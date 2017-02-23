c7ee6177e19a33614e8c8f22773c99439e0dfc27
Miryam Chaabouni <miryam.chaabouni@itk-engineering.de>
2017-02-23 18:02:21 +0100

    Minor correction to Canteen menu

--- ItkPortalApplication/src/main/java/com/itk/chaabouni/dao/fillDataBase.java
+++ ItkPortalApplication/src/main/java/com/itk/chaabouni/dao/fillDataBase.java
@@ -36,19 +36,45 @@ public class fillDataBase {
         //********************************************************************
         MealService ms = new MealService(); 
         Meal meal1 = new Meal();
+        Meal meal2 = new Meal(); // default meal
+
+        // Monday menu 
+        meal1.setId(100);
+        
+        meal2.setId(101);
+        meal2.setName("Pizza");
+        meal2.setDescription("with ham and mushrooms");
+        meal2.setVegetarian(false);
+        
+        ms.addMenu(new Menu("Monday", meal1, meal2));
+        
+        // Tuesday menu 
         meal1.setId(102);
         meal1.setName("Salmon ");
         meal1.setDescription("with side vegetables");
         meal1.setVegetarian(false);
         meal1.setPrice(10.0);
         
-        Meal meal2 = new Meal(); // default meal
         meal2.setId(103);
         meal2.setName("Lentils soup ");
         meal2.setDescription(" with carrots and cottage cheese");
+        meal2.setVegetarian(true);
         
         ms.addMenu(new Menu("Tuesday", meal1, meal2));
         
+        // Wednesday's menu 
+        meal1.setId(104);
+        meal1.setName("Cheese burger ");
+        meal1.setDescription("with fried potatoes");
+        meal1.setVegetarian(false);
+        meal1.setPrice(8.0);
+        
+        meal2.setId(105);
+        meal2.setName("Spinach Lasagna");
+        meal2.setDescription("with cream sauce and Grana Padano");
+        meal2.setVegetarian(true);
+        
+        ms.addMenu(new Menu("Wednesday", meal1, meal2));
     }
     
 }
