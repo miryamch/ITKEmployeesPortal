e370ab5f529218b1a38e0d36eeb2a5b7951c99a3
Miryam Chaabouni <miryam.chaabouni@gmail.com>
2017-02-24 14:28:16 +0100

    Added some tests

---
+++ ItkPortalApplication/src/test/java/com/itk/chaabouni/dto/MealTest.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itk.chaabouni.dto;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mchaabouni
+ */
+public class MealTest {
+    private final int DEFAULT_ID = 100;
+    private final String DEFAULT_NAME = "Pasta";
+    private final String DEFAULT_DESCRIPTION = "food";
+    private final String DEFAULT_WEEKDAY = "Monday";
+    private final Double DEFAULT_PRICE = 4.5;
+    private final boolean DEFAULT_VEGETARIAN = true;
+  
+    
+    
+    public MealTest() {
+    }
+  
+    /**
+     * Test of getId method, of class Meal.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        int expResult = DEFAULT_ID;
+        Meal mealInstance = new Meal(expResult, DEFAULT_NAME,
+                DEFAULT_DESCRIPTION, DEFAULT_WEEKDAY, DEFAULT_PRICE , 
+                DEFAULT_VEGETARIAN);
+        int result = mealInstance.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class Meal.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        int id = DEFAULT_ID;
+        Meal instance = new Meal();
+        instance.setId(id);
+        assertEquals(id, instance.getId());
+    }
+
+    /**
+     * Test of getName method, of class Meal.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        String expResult = DEFAULT_NAME;
+        Meal instance = new Meal(DEFAULT_ID, expResult,
+                DEFAULT_DESCRIPTION, DEFAULT_WEEKDAY, DEFAULT_PRICE , 
+                DEFAULT_VEGETARIAN);
+        String result = instance.getName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setName method, of class Meal.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String Name = DEFAULT_NAME;
+        Meal instance = new Meal();
+        instance.setName(Name);
+        assertEquals(Name, instance.getName());        
+    }
+
+    /**
+     * Test of getDescription method, of class Meal.
+     */
+    @Test
+    public void testGetDescription() {
+        System.out.println("getDescription");
+        String expResult = DEFAULT_DESCRIPTION;        
+        Meal instance = new Meal(DEFAULT_ID, DEFAULT_NAME,
+                expResult, DEFAULT_WEEKDAY, DEFAULT_PRICE , 
+                DEFAULT_VEGETARIAN);
+        String result = instance.getDescription();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setDescription method, of class Meal.
+     */
+    @Test
+    public void testSetDescription() {
+        System.out.println("setDescription");
+        String Description = DEFAULT_DESCRIPTION;
+        Meal instance = new Meal();
+        instance.setDescription(Description);
+        assertEquals(Description, instance.getDescription());
+    }
+
+    /**
+     * Test of getPrice method, of class Meal.
+     */
+    @Test
+    public void testGetPrice() {
+        System.out.println("getPrice");
+        Double expResult = null;
+        Meal instance = new Meal(DEFAULT_ID, DEFAULT_NAME,
+                DEFAULT_DESCRIPTION, DEFAULT_WEEKDAY, expResult , 
+                DEFAULT_VEGETARIAN);
+        Double result = instance.getPrice();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setPrice method, of class Meal.
+     */
+    @Test
+    public void testSetPrice() {
+        System.out.println("setPrice");
+        Double price = null;
+        Meal instance = new Meal();
+        instance.setPrice(price);
+        assertEquals(price, instance.getPrice());
+    }
+
+    /**
+     * Test of isVegetarian method, of class Meal.
+     */
+    @Test
+    public void testIsVegetarian() {
+        System.out.println("isVegetarian");
+        boolean expResult = false;
+        Meal instance = new Meal(DEFAULT_ID, DEFAULT_NAME,
+                DEFAULT_DESCRIPTION, DEFAULT_WEEKDAY, DEFAULT_PRICE , 
+                expResult);
+        boolean result = instance.isVegetarian();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setVegetarian method, of class Meal.
+     */
+    @Test
+    public void testSetVegetarian() {
+        System.out.println("setVegetarian");
+        boolean vegetarian = false;
+        Meal instance = new Meal();
+        instance.setVegetarian(vegetarian);
+        assertEquals(vegetarian, instance.isVegetarian());
+    }
+    
+}
---
+++ ItkPortalApplication/src/test/java/com/itk/chaabouni/dto/MenuTest.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itk.chaabouni.dto;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mchaabouni
+ */
+public class MenuTest {
+    
+    private static final String DEFAULT_DAY = "Monday"; 
+    private static final Meal DEFAULT_MEAL1 = new Meal() ; 
+    private static final Meal DEFAULT_MEAL2 = new Meal(); 
+    
+    public MenuTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDay method, of class Menu.
+     */
+    @Test
+    public void testGetDay() {
+        System.out.println("getDay");
+        String expResult = DEFAULT_DAY;
+        Menu instance = new Menu(expResult, DEFAULT_MEAL1, DEFAULT_MEAL2);
+        String result = instance.getDay();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setDay method, of class Menu.
+     */
+    @Test
+    public void testSetDay() {
+        System.out.println("setDay");
+        String day = DEFAULT_DAY;
+        Menu instance = new Menu();
+        instance.setDay(day);
+        assertEquals(instance.getDay(), day);
+    }
+
+    /**
+     * Test of getMeal1 method, of class Menu.
+     */
+    @Test
+    public void testGetMeal1() {
+        System.out.println("getMeal1");
+        Meal expResult =  DEFAULT_MEAL1;
+        Menu instance = new Menu(DEFAULT_DAY, expResult, DEFAULT_MEAL2);
+        Meal result = instance.getMeal1();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setMeal1 method, of class Menu.
+     */
+    @Test
+    public void testSetMeal1() {
+        System.out.println("setMeal1");
+        Meal meal1 = null;
+        Menu instance = new Menu();
+        instance.setMeal1(meal1);
+        assertEquals(instance.getMeal1(), meal1);
+    }
+
+    /**
+     * Test of getMeal2 method, of class Menu.
+     */
+    @Test
+    public void testGetMeal2() {
+        System.out.println("getMeal2");
+        Meal expResult = DEFAULT_MEAL2;
+        Menu instance = new Menu(DEFAULT_DAY, DEFAULT_MEAL1, expResult);
+        Meal result = instance.getMeal2();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setMeal2 method, of class Menu.
+     */
+    @Test
+    public void testSetMeal2() {
+        System.out.println("setMeal2");
+        Meal meal2 = null;
+        Menu instance = new Menu();
+        instance.setMeal2(meal2);
+        assertEquals(instance.getMeal2(), meal2);
+    }
+
+    /**
+     * Test of getMeals method, of class Menu.
+     */
+    @Test
+    public void testGetMeals() {
+        System.out.println("getMeals");
+        Menu instance = new Menu(DEFAULT_DAY, DEFAULT_MEAL1, DEFAULT_MEAL2);
+        ArrayList<Meal> result = instance.getMeals();
+        assertFalse(result.isEmpty());
+        assertTrue(result.size()==2);
+    }
+    
+}
