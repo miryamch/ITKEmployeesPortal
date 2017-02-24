babd2beb283f45a9e3f26b5d2b12ffec772b4274
Miryam Chaabouni <miryam.chaabouni@gmail.com>
2017-02-24 14:17:45 +0100

    added some test units #

--- ItkPortalApplication/src/test/java/com/itk/chaabouni/dto/EmployeeTest.java
+++ ItkPortalApplication/src/test/java/com/itk/chaabouni/dto/EmployeeTest.java
@@ -1,10 +1,11 @@
+package com.itk.chaabouni.dto;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import com.itk.chaabouni.dto.Employee;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test; 
 /**
