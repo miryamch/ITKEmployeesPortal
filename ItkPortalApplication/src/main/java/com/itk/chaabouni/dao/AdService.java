bad9dea362584cad4a7aef53f4085ae3aecc974d
Miryam Chaabouni <miryam.chaabouni@gmail.com>
2017-02-27 16:55:11 +0100

    Bugfix: date in Claddified ads

--- ItkPortalApplication/src/main/java/com/itk/chaabouni/dao/AdService.java
+++ ItkPortalApplication/src/main/java/com/itk/chaabouni/dao/AdService.java
@@ -1,17 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package com.itk.chaabouni.dao;
 
 import com.itk.chaabouni.dto.ClassifiedAd;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,7 +27,7 @@ import java.util.List;
 @Named(value = "adService")
 @SessionScoped
 public class AdService implements Serializable {
-
+    
     /**
      * Creates a new instance of AdService
      */
@@ -28,11 +35,19 @@ public class AdService implements Serializable {
     }
     
     public List<ClassifiedAd> getListOfAds() {
-        ArrayList<ClassifiedAd> outList = new ArrayList<>(); 
-        Date date = new Date(2017, 2, 27);
-        ClassifiedAd ad = new ClassifiedAd("John Doe", date, "Used iPhone 4", "Good condition. With Charger."); 
-        outList.add(ad); 
-        return outList; 
+        ArrayList<ClassifiedAd> outList = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date(); 
+        try {
+            date = sdf.parse("27/02/2017");
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(AdService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        ClassifiedAd ad = new ClassifiedAd("John Doe", date, "Used iPhone 4", "Good condition. With Charger.");
+        outList.add(ad);
+        return outList;
     }
     
     public void addAd(ClassifiedAd newAd){
